b9821edd1d58fce22f55b25957c3c4f09474b82f
Chris Enedah <ikezue@users.noreply.github.com>
2021-09-09 18:08:26 -0700

    Add a server

---
+++ app/app.js
@@ -0,0 +1,47 @@
+import { default as express } from "express";
+import { default as hbs } from "hbs";
+import * as path from "path";
+import { default as logger } from "morgan";
+import { default as cookieParser } from "cookie-parser";
+import * as http from "http";
+import { appDir } from "./util.js";
+const __dirname = appDir;
+import {
+  normalizePort,
+  onError,
+  onListening,
+  handle404,
+  basicErrorHandler,
+} from "./util.js";
+
+import { router as indexRouter } from "./routes/index.js";
+
+export const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname, "partials"));
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Router function lists
+app.use("/", indexRouter);
+
+// error handlers
+// catch 404 and forward to error handler
+app.use(handle404);
+app.use(basicErrorHandler);
+
+export const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+export const server = http.createServer(app);
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
---
+++ app/util.js
@@ -0,0 +1,58 @@
+import * as path from "path";
+import * as url from "url";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export const appDir = __dirname;
+
+export function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  } else if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+export function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+import { server } from "./app.js";
+export function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  console.log(`Listening on ${bind}`);
+}
+
+import createError from "http-errors";
+export function handle404(req, res, next) {
+  next(createError(404));
+}
+
+export function basicErrorHandler(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+}
